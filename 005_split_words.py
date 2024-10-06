5af380e163e2a9b29d2fa9b82fcbc512b90d30d3
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2024-10-06 16:15:15 -0400

    Add files via upload

---
+++ 005_split_words.py
@@ -0,0 +1,24 @@
+# split_words.py
+
+words = 'Tabitha Lee'
+
+splitWords = words.split()
+
+for z in splitWords:
+    print(z)
+
+input('')
+
+##
+
+'''
+Tabitha
+Lee
+'''
+
+# Dedicated to God the Father
+# All Rights Reserved Christopher Andrew Topalian Copyright 2000-2024
+# https://github.com/ChristopherTopalian
+# https://github.com/ChristopherAndrewTopalian
+# https://sites.google.com/view/CollegeOfScripting
+
