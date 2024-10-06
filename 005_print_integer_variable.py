0ce05f927f030e4e8e11b67eb5d875b03215a9d7
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2024-10-06 10:55:15 -0400

    Add files via upload

---
+++ 005_print_integer_variable.py
@@ -0,0 +1,20 @@
+# print_integer_variable.py
+
+numberOfPeople = 20
+
+print(numberOfPeople)
+
+input('')
+
+##
+
+'''
+20
+'''
+
+# Dedicated to God the Father
+# All Rights Reserved Christopher Andrew Topalian Copyright 2000-2024
+# https://github.com/ChristopherTopalian
+# https://github.com/ChristopherAndrewTopalian
+# https://sites.google.com/view/CollegeOfScripting
+
