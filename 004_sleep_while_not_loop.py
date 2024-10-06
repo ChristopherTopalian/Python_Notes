2e4e537403fa0edf4aa52fc685a47d92e2160d2b
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2024-10-06 15:22:04 -0400

    Add files via upload

---
+++ 004_sleep_while_not_loop.py
@@ -0,0 +1,29 @@
+# sleep_while_not_loop.py
+
+import time
+
+loopCondition = 0
+
+print('Count to 4')
+
+while loopCondition != 1:
+    time.sleep(4.0)
+    print('4 seconds passed')
+
+input('')
+
+##
+
+'''
+Count to 4
+4 seconds passed
+4 seconds passed
+4 seconds passed
+'''
+
+# Dedicated to God the Father
+# All Rights Reserved Christopher Andrew Topalian Copyright 2000-2024
+# https://github.com/ChristopherTopalian
+# https://github.com/ChristopherAndrewTopalian
+# https://sites.google.com/view/CollegeOfScripting
+
