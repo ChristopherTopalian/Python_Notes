908478f930fe3cc69dcb04619b413517034e5461
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2024-10-06 16:26:25 -0400

    Add files via upload

---
+++ 006_slice_start_stop.py
@@ -0,0 +1,20 @@
+# slice_start_stop.py
+
+people = ['Melissa', 'Jennifer', 'Tabitha', 'Jane']
+
+# start at 0, stop is 2
+print(people[0:2])
+
+input('')
+
+'''
+       0                1
+['Melissa', 'Jennifer']
+'''
+
+# Dedicated to God the Father
+# All Rights Reserved Christopher Andrew Topalian Copyright 2000-2024
+# https://github.com/ChristopherTopalian
+# https://github.com/ChristopherAndrewTopalian
+# https://sites.google.com/view/CollegeOfScripting
+
