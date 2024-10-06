8bdc86d7e5e1c9457b304cf679723e40459aaeab
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2024-10-06 11:21:12 -0400

    Add files via upload

---
+++ 007_print_dictionary_variable.py
@@ -0,0 +1,23 @@
+# print_dictionary_variable.py
+
+jane = {
+    'name': 'Jane',
+    'score': 98
+}
+
+print(jane)
+
+input('')
+
+##
+
+'''
+{'name': 'Jane', 'score': 98}
+'''
+
+# Dedicated to God the Father
+# All Rights Reserved Christopher Andrew Topalian Copyright 2000-2024
+# https://github.com/ChristopherTopalian
+# https://github.com/ChristopherAndrewTopalian
+# https://sites.google.com/view/CollegeOfScripting
+
