658b857f3e2db3e63224e2e36aa40f44e22cafe8
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2024-10-06 11:08:34 -0400

    Add files via upload

---
+++ 006_print_float_variable.py
@@ -0,0 +1,20 @@
+# print_float_variable.py
+
+numberOfPeople = 20.82
+
+print(numberOfPeople)
+
+input('')
+
+##
+
+'''
+20.82
+'''
+
+# Dedicated to God the Father
+# All Rights Reserved Christopher Andrew Topalian Copyright 2000-2024
+# https://github.com/ChristopherTopalian
+# https://github.com/ChristopherAndrewTopalian
+# https://sites.google.com/view/CollegeOfScripting
+
