bd877fc61648d43f7ae454c3dcc0c68714924e8e
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2024-10-06 10:34:08 -0400

    Add files via upload

---
+++ 004_print_string_variable.py
@@ -0,0 +1,19 @@
+# print_string_variable.py
+
+numberOfPeople = 'Twenty'
+
+print(numberOfPeople)
+
+input('')
+
+##
+
+'''
+Twenty
+'''
+
+# Dedicated to God the Father
+# All Rights Reserved Christopher Andrew Topalian Copyright 2000-2024
+# https://github.com/ChristopherTopalian
+# https://github.com/ChristopherAndrewTopalian
+
