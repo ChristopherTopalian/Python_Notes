2f7e25de91b006d8e0e72f1c9816025465a5a221
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2024-10-05 20:19:02 -0400

    Add files via upload

---
+++ 003_sort_list_append.py
@@ -0,0 +1,26 @@
+# sort_list_append.py
+
+names = ['Melissa', 'Jennifer', 'Tabitha', 'Jane']
+
+names.sort()
+
+theNames = []
+
+for z in names:
+    theNames.append(z)
+
+print(theNames)
+
+input('')
+
+##
+
+'''
+['Jane', 'Jennifer', 'Melissa', 'Tabitha']
+'''
+
+# Dedicated to God the Father
+# All Rights Reserved Christopher Andrew Topalian Copyright 2000-2024
+# https://github.com/ChristopherTopalian
+# https://github.com/ChristopherAndrewTopalian
+
