5adf6b2dc19d318b4b487aa838a3285aaf1c63aa
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2024-10-06 07:21:40 -0400

    Add files via upload

---
+++ 001_print.py
@@ -0,0 +1,18 @@
+# print.py
+
+print('Hi Everyone')
+
+input('Press Enter to Exit')
+
+##
+
+'''
+Hi Everyone
+Press Enter to Exit
+'''
+
+# Dedicated to God the Father
+# All Rights Reserved Christopher Andrew Topalian Copyright 2000-2024
+# https://github.com/ChristopherTopalian
+# https://github.com/ChristopherAndrewTopalian
+
