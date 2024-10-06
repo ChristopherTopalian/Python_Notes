2f8e3d7cad911dc2af28c1bb3ffa231b8b671e25
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2024-10-06 09:12:15 -0400

    Add files via upload

---
+++ 002_input_username.py
@@ -0,0 +1,19 @@
+# input_username.py
+
+userName = input('Enter Username: ')
+
+print('Username is: ' + userName)
+
+input('')
+
+##
+
+'''
+Username is: Christopher
+'''
+
+# Dedicated to God the Father
+# All Rights Reserved Christopher Andrew Topalian Copyright 2000-2024
+# https://github.com/ChristopherTopalian
+# https://github.com/ChristopherAndrewTopalian
+
