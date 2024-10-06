7b587c817db01898673fee42f328520d08a4a48e
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2024-10-06 15:12:11 -0400

    Add files via upload

---
+++ 003_sleep_input_while.py
@@ -0,0 +1,28 @@
+# sleep_input_while.py
+
+import time
+
+startCondition = input('Type 1 and press Enter\n')
+
+while startCondition == '1':
+    time.sleep(4.0)
+    print('4 seconds passed')
+
+input('')
+
+##
+
+'''
+Type 1 and press Enter
+1
+4 seconds passed
+4 seconds passed
+4 seconds passed
+'''
+
+# Dedicated to God the Father
+# All Rights Reserved Christopher Andrew Topalian Copyright 2000-2024
+# https://github.com/ChristopherTopalian
+# https://github.com/ChristopherAndrewTopalian
+# https://sites.google.com/view/CollegeOfScripting
+
