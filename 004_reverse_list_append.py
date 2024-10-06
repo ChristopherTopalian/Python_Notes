9a1ae2811fc7538cd88a0736555dd25257a06365
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2024-10-05 20:52:51 -0400

    Add files via upload

---
+++ 004_reverse_list_append.py
@@ -0,0 +1,26 @@
+# reverse_list_append.py
+
+names = ['Melissa', 'Jennifer', 'Tabitha', 'Jane']
+
+names.reverse()
+
+theNames = []
+
+for z in names:
+    theNames.append(z)
+
+print(theNames)
+
+input('')
+
+##
+
+'''
+['Jane', 'Tabitha', 'Jennifer', 'Melissa']
+'''
+
+# Dedicated to God the Father
+# All Rights Reserved Christopher Andrew Topalian Copyright 2000-2024
+# https://github.com/ChristopherTopalian
+# https://github.com/ChristopherAndrewTopalian
+
