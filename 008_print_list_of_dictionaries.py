c6fc4f7c745084568f6fa6a094a4426d3ab648ac
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2024-10-06 11:29:32 -0400

    Add files via upload

---
+++ 008_print_list_of_dictionaries.py
@@ -0,0 +1,34 @@
+# print_list_of_dictionaries.py
+
+people = [
+    {
+        'name': 'Jane',
+        'score': 95
+    },
+    {
+        'name': 'Melissa',
+        'score': 98
+    },
+    {
+        'name': 'Tabitha',
+        'score': 93
+    }
+]
+
+print(people)
+
+input('')
+
+##
+
+'''
+[{'name': 'Jane', 'score': 95}, {'name': 'Melissa', 'score': 98}, {'name': 
+'Tabitha', 'score': 93}]
+'''
+
+# Dedicated to God the Father
+# All Rights Reserved Christopher Andrew Topalian Copyright 2000-2024
+# https://github.com/ChristopherTopalian
+# https://github.com/ChristopherAndrewTopalian
+# https://sites.google.com/view/CollegeOfScripting
+
