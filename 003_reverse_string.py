491a3a5c2faca13aab096797663d887ab39395e0
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2024-10-06 15:54:24 -0400

    Add files via upload

---
+++ 003_reverse_string.py
@@ -0,0 +1,22 @@
+# reverse_string.py
+
+name = "kingdom"
+
+# slicing, start, stop, step
+reversedName = name[::-1]
+
+print(reversedName)
+
+input('')
+
+##
+
+'''
+modgnik
+'''
+
+# Dedicated to God the Father
+# All Rights Reserved Christopher Andrew Topalian Copyright 2000-2024
+# https://github.com/ChristopherTopalian
+# https://sites.google.com/view/CollegeOfScripting
+
