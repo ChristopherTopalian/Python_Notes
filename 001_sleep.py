99e5984fb763e26b182655ad14fbb204a776c16f
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2024-10-06 13:22:16 -0400

    Add files via upload

---
+++ 001_sleep.py
@@ -0,0 +1,25 @@
+# sleep.py
+
+import time
+
+print('Count to 4')
+
+time.sleep(4.0)
+
+print('4 seconds passed')
+
+input('')
+
+##
+
+'''
+Count to 4
+4 seconds passed
+'''
+
+# Dedicated to God the Father
+# All Rights Reserved Christopher Andrew Topalian Copyright 2000-2024
+# https://github.com/ChristopherTopalian
+# https://github.com/ChristopherAndrewTopalian
+# https://sites.google.com/view/CollegeOfScripting
+
