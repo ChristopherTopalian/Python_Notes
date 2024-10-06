e1200c150a3a01e5297ba5b3ce0a9a8000f5839e
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2024-10-06 08:39:13 -0400

    Add files via upload

---
+++ count_letters_variable.py
@@ -0,0 +1,21 @@
+# count_letters_variable.py
+
+name = 'Jane'
+
+letterCount = len(name)
+
+print(letterCount)
+
+input('')
+
+##
+
+'''
+4
+'''
+
+# Dedicated to God the Father
+# All Rights Reserved Christopher Andrew Topalian Copyright 2000-2024
+# https://github.com/ChristopherTopalian
+# https://github.com/ChristopherAndrewTopalian
+
