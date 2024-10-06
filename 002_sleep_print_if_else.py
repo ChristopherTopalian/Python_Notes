8cc7c662ba0e1a7d20e58aa9a3ed63a82e8f4b2c
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2024-10-06 14:57:09 -0400

    Add files via upload

---
+++ 002_sleep_print_if_else.py
@@ -0,0 +1,39 @@
+# sleep_print_if_else.py
+
+import time
+
+print('Hi Friend')
+
+time.sleep(3.0)
+
+print('Is the sun shining?')
+
+sunShining = input('y/n\n')
+
+if (sunShining == 'y'):
+    print('Nice that it is sunny out')
+else:
+    print('The sun will be there soon')
+
+input('')
+
+##
+
+'''
+Hi Friend
+Is the sun shining?
+y/n
+
+if y
+Nice that it is sunny out
+
+if n
+The sun will be there soon
+'''
+
+# Dedicated to God the Father
+# All Rights Reserved Christopher Andrew Topalian Copyright 2000-2024
+# https://github.com/ChristopherTopalian
+# https://github.com/ChristopherAndrewTopalian
+# https://sites.google.com/view/CollegeOfScripting
+
