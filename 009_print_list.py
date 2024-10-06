fc566054b21d7676d32157ba4d6bf24ecadce6eb
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2024-10-06 11:35:12 -0400

    Add files via upload

---
+++ 009_print_list.py
@@ -0,0 +1,24 @@
+# print_list.py
+
+people = [
+    'Jane',
+    'Tabitha',
+    'Melissa'
+]
+
+print(people)
+
+input('')
+
+##
+
+'''
+['Jane', 'Tabitha', 'Melissa']
+'''
+
+# Dedicated to God the Father
+# All Rights Reserved Christopher Andrew Topalian Copyright 2000-2024
+# https://github.com/ChristopherTopalian
+# https://github.com/ChristopherAndrewTopalian
+# https://sites.google.com/view/CollegeOfScripting
+
