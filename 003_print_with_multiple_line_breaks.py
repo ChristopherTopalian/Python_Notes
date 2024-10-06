1e8f9a7eb364d562334cde40838878772b9f7e63
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2024-10-06 08:00:43 -0400

    Add files via upload

---
+++ 003_print_with_multiple_line_breaks.py
@@ -0,0 +1,19 @@
+# print_with_multiple_line_breaks.py
+
+print('Hi Everyone\n\nHappy Scripting')
+
+input('')
+
+##
+
+'''
+Hi Everyone    
+
+Happy Scripting
+'''
+
+# Dedicated to God the Father
+# All Rights Reserved Christopher Andrew Topalian Copyright 2000-2024
+# https://github.com/ChristopherTopalian
+# https://github.com/ChristopherAndrewTopalian
+
