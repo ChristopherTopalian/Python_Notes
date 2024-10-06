ffae879a10e6ffe5ded8a4cc8bba7b106ef22e71
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2024-10-06 18:22:54 -0400

    Add files via upload

---
+++ slice/slice_start_stop.js
@@ -0,0 +1,22 @@
+// slice_start_stop.js
+
+let people = ["Melissa", "Jennifer", "Tabitha", "Jane"];
+
+// start and stop indices
+let start = 0;
+let stop = 2;
+
+// loop from start to stop, printing each element
+for (let i = start; i < stop; i++)
+{
+    console.log(people[i]);
+}
+
+//--//
+
+// Dedicated to God the Father
+// All Rights Reserved Christopher Andrew Topalian Copyright 2000-2024
+// https://github.com/ChristopherTopalian
+// https://github.com/ChristopherAndrewTopalian
+// https://sites.google.com/view/CollegeOfScripting
+
