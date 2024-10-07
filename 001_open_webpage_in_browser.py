d399f0a828cf9b4a8ab944396102d87b1ca3cbb0
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2024-10-06 20:47:14 -0400

    Add files via upload

---
+++ 001_open_webpage_in_browser.py
@@ -0,0 +1,17 @@
+# open_webpage_in_browser.py
+
+import webbrowser
+
+webbrowser.open('https://github.com/ChristopherTopalian')
+
+##
+
+'''
+Opens Default Web Browser to a Web Page.
+'''
+
+# Dedicated to God the Father
+# All Rights Reserved Christopher Andrew Topalian Copyright 2000-2024
+# https://github.com/ChristopherTopalian
+# https://sites.google.com/view/CollegeOfScripting
+
