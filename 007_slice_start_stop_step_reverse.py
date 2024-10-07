426329294a719df2964bb2a8c22fc5cde59dea77
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2024-10-06 20:40:29 -0400

    Add files via upload

---
+++ 007_slice_start_stop_step_reverse.py
@@ -0,0 +1,20 @@
+# slice_start_stop_step_reverse.py
+
+people = ['Melissa', 'Jennifer', 'Tabitha', 'Jane']
+
+# start, stop, step
+print(people[3:1:-1])
+
+input('')
+
+##
+
+'''
+['Jane', 'Tabitha']
+'''
+
+# Dedicated to God the Father
+# All Rights Reserved Christopher Andrew Topalian Copyright 2000-2024
+# https://github.com/ChristopherTopalian
+# https://sites.google.com/view/CollegeOfScripting
+
