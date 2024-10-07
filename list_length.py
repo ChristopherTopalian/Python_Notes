54a40649f50d04bf776e797c7d46efb6d07ccf20
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2024-10-06 20:29:13 -0400

    Add files via upload

---
+++ list_length.py
@@ -0,0 +1,23 @@
+# list_length.py
+
+people = [
+    'Jane',
+    'Tabitha'
+]
+
+numberOfPeople = len(people)
+
+print(f'{numberOfPeople} people')
+
+##
+
+'''
+2
+'''
+
+# Dedicated to God the Father
+# All Rights Reserved Christopher Andrew Topalian Copyright 2000-2024
+# https://github.com/ChristopherTopalian
+# https://github.com/ChristopherAndrewTopalian
+# https://sites.google.com/view/CollegeOfScripting
+
