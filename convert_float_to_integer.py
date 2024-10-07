6eda39c5bc50960e8b1c58a1d9d7dc0afdc5961e
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2024-10-07 18:46:38 -0400

    Add files via upload

---
+++ convert_float_to_integer.py
@@ -0,0 +1,22 @@
+# convert_float_to_integer.py
+
+theNumber = 22.74
+
+convertedNumber = int(theNumber)
+
+print(convertedNumber)
+
+input('')
+
+##
+
+'''
+22
+'''
+
+# Dedicated to God the Father
+# All Rights Reserved Christopher Andrew Topalian Copyright 2000-2024
+# https://github.com/ChristopherTopalian
+# https://github.com/ChristopherAndrewTopalian
+# https://sites.google.com/view/CollegeOfScripting
+
