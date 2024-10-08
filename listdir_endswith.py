e1686c2fb2df3f11c48b87a0420b140e5e9c84f1
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2024-10-07 21:41:36 -0400

    Add files via upload

---
+++ listdir_endswith.py
@@ -0,0 +1,25 @@
+# listdir_endswith.py
+
+import os
+
+folderContents = os.listdir('.')
+
+for z in folderContents:
+    if z.endswith('.py'):
+        print(z)
+
+input('')
+
+##
+
+'''
+listdir.py
+listdir_endswith.py
+'''
+
+# Dedicated to God the Father
+# All Rights Reserved Christopher Andrew Topalian Copyright 2000-2024
+# https://github.com/ChristopherTopalian
+# https://github.com/ChristopherAndrewTopalian
+# https://sites.google.com/view/CollegeOfScripting
+
