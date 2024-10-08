a2d6f181503191352faac1d207300793093ecc00
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2024-10-07 21:16:00 -0400

    Add files via upload

---
+++ listdir.py
@@ -0,0 +1,22 @@
+# listdir.py
+
+import os
+
+folderContents = os.listdir()
+
+print(folderContents)
+
+input('')
+
+##
+
+'''
+['output.txt', 'py', 'Python_Computer_Science.md', 'style001.css']
+'''
+
+# Dedicated to God the Father
+# All Rights Reserved Christopher Andrew Topalian Copyright 2000-2024
+# https://github.com/ChristopherTopalian
+# https://github.com/ChristopherAndrewTopalian
+# https://sites.google.com/view/CollegeOfScripting
+
