1fe19f1d755bfde46729508614aa677529a36569
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2024-10-18 03:17:20 -0400

    Add files via upload

---
+++ calendar_specified_year_month.py
@@ -0,0 +1,21 @@
+# calendar_specified_year_month.py
+
+import calendar
+
+# specify month and year
+year = 2029
+month = 4
+
+# show the calendar for April 2029
+print(calendar.month(year, month))
+
+input('')
+
+##
+
+# Dedicated to God the Father
+# All Rights Reserved Christopher Andrew Topalian Copyright 2000-2024
+# https://github.com/ChristopherTopalian
+# https://github.com/ChristopherAndrewTopalian
+# https://sites.google.com/view/CollegeOfScripting
+
