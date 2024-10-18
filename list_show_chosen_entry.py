d63909f4f74743a5cf3eef655985ac4127f48af7
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2024-10-18 06:32:46 -0400

    Add files via upload

---
+++ list_show_chosen_entry.py
@@ -0,0 +1,27 @@
+# list_show_chosen_entry.py
+
+ourList = [0, 1, 2, 3, 4, 5]
+
+ourChoice = int(input('Enter 0 to 5: \n'))
+
+print('We chose:', ourList[ourChoice])
+
+input('')
+
+####
+
+'''
+Enter 0 to 5: 
+4
+We chose: 4
+'''
+
+# we can alternatively use:
+# print(f'We chose: {ourList[ourChoice]}')
+
+# Dedicated to God the Father
+# All Rights Reserved Christopher Andrew Topalian Copyright 2000-2024
+# https://github.com/ChristopherTopalian
+# https://github.com/ChristopherAndrewTopalian
+# https://sites.google.com/view/CollegeOfScripting
+
