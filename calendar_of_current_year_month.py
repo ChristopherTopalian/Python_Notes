e646c526ce312404cf43fac7557d2d2deb9da351
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2024-10-18 03:05:13 -0400

    Add files via upload

---
+++ calendar_of_current_year_month.py
@@ -0,0 +1,25 @@
+# calendar_of_current_year_month.py
+
+import calendar as cal
+import datetime as dt
+
+theYear = dt.date.today().year
+theMonth = dt.date.today().month
+
+print(cal.month(theYear, theMonth))
+
+input('')
+
+##
+
+'''
+Shows a calendar for the
+current year and month
+'''
+
+# Dedicated to God the Father
+# All Rights Reserved Christopher Andrew Topalian Copyright 2000-2024
+# https://github.com/ChristopherTopalian
+# https://github.com/ChristopherAndrewTopalian
+# https://sites.google.com/view/CollegeOfScripting
+
