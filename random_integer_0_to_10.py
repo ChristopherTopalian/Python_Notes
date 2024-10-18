cb4bd2a949a8506d980cd1109b42504aa45e4380
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2024-10-18 11:16:23 -0400

    Add files via upload

---
+++ random_integer_0_to_10.py
@@ -0,0 +1,23 @@
+# random_integer_0_to_10.py
+
+import random
+
+# integer from 1 to 10
+randomInteger = random.randint(1, 10)
+
+print(randomInteger)
+
+input('')
+
+####
+
+'''
+7
+'''
+
+# Dedicated to God the Father
+# All Rights Reserved Christopher Andrew Topalian Copyright 2000-2024
+# https://github.com/ChristopherTopalian
+# https://github.com/ChristopherAndrewTopalian
+# https://sites.google.com/view/CollegeOfScripting
+
