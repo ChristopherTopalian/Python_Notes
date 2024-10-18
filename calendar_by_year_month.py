351fbe64904551d89773da5f5be01384077a9a30
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2024-10-17 22:02:26 -0400

    Add files via upload

---
+++ calendar_by_year_month.py
@@ -0,0 +1,19 @@
+# calendar_by_year_month.py
+
+import calendar
+
+theYear = int(input("Enter Year: "))
+theMonth = int(input("Enter Month: "))
+
+print(calendar.month(theYear, theMonth))
+
+input('')
+
+##
+
+# Dedicated to God the Father
+# All Rights Reserved Christopher Andrew Topalian Copyright 2000-2024
+# https://github.com/ChristopherTopalian
+# https://github.com/ChristopherAndrewTopalian
+# https://sites.google.com/view/CollegeOfScripting
+
