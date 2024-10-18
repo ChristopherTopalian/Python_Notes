a8d9ccabecb5c7e5004023968e1c0ceda11f1f0a
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2024-10-18 12:52:15 -0400

    Add files via upload

---
+++ get_keyword_list.py
@@ -0,0 +1,23 @@
+# get_keyword_list.py
+
+import keyword
+
+keywordList = keyword.kwlist
+
+print(keywordList)
+
+input('')
+
+####
+
+'''
+['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 
+'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+'''
+
+# Dedicated to God the Father
+# All Rights Reserved Christopher Andrew Topalian Copyright 2000-2024
+# https://github.com/ChristopherTopalian
+# https://github.com/ChristopherAndrewTopalian
+# https://sites.google.com/view/CollegeOfScripting
+
