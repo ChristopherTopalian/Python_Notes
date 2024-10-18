5ba0f2ec96f951c52b3ee82179d4c8eb8ba30b23
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2024-10-18 11:09:58 -0400

    Add files via upload

---
+++ randomFloat0to1.py
@@ -0,0 +1,28 @@
+# randomFloat0to1.py
+
+import random
+
+def randomFloat0to1():
+    # float from 0.0 to 1.0
+    randomFloat = random.random()
+    return randomFloat
+
+##
+
+if __name__ == "__main__":
+    print(randomFloat0to1())
+
+    input('')
+
+####
+
+'''
+0.3271998297830063
+'''
+
+# Dedicated to God the Father
+# All Rights Reserved Christopher Andrew Topalian Copyright 2000-2024
+# https://github.com/ChristopherTopalian
+# https://github.com/ChristopherAndrewTopalian
+# https://sites.google.com/view/CollegeOfScripting
+
