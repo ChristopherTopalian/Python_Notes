bb3148f1149050ef89793e9daefdc3fd821a78fa
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2024-10-18 05:48:58 -0400

    Add files via upload

---
+++ list_show_first_entry.py
@@ -0,0 +1,16 @@
+# list_show_first_entry.py
+
+ourList = [0, 1, 2, 3, 4, 5]
+
+print(ourList[0])
+
+input('')
+
+####
+
+# Dedicated to God the Father
+# All Rights Reserved Christopher Andrew Topalian Copyright 2000-2024
+# https://github.com/ChristopherTopalian
+# https://github.com/ChristopherAndrewTopalian
+# https://sites.google.com/view/CollegeOfScripting
+
