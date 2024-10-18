45b6c4d47a2fedd2d6ca80e5a38bcbc4c2126380
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2024-10-18 07:54:53 -0400

    Add files via upload

---
+++ random_float_0_to_1.py
@@ -0,0 +1,22 @@
+# random_float_0_to_1.py
+
+import random
+
+randomFloat = random.random()
+
+print(randomFloat)
+
+input('')
+
+####
+
+'''
+0.5109827327232547
+'''
+
+# Dedicated to God the Father
+# All Rights Reserved Christopher Andrew Topalian Copyright 2000-2024
+# https://github.com/ChristopherTopalian
+# https://github.com/ChristopherAndrewTopalian
+# https://sites.google.com/view/CollegeOfScripting
+
