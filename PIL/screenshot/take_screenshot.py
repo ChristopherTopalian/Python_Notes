5d178b65b71fb5802486e0358ae30b6e2cf40c9f
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2024-10-19 12:19:04 -0400

    Add files via upload

---
+++ PIL/screenshot/take_screenshot.py
@@ -0,0 +1,21 @@
+# take_screenshot.py
+
+from PIL import ImageGrab
+
+# capture the screen
+ourImage = ImageGrab.grab()
+
+# save screenshot as .png
+ourImage.save('ourNewTexture.png')
+
+####
+
+# Requires: Pillow
+# Install: pip install Pillow
+
+# Dedicated to God the Father
+# All Rights Reserved Christopher Andrew Topalian Copyright 2000-2024
+# https://github.com/ChristopherTopalian
+# https://github.com/ChristopherAndrewTopalian
+# https://sites.google.com/view/CollegeOfScripting
+
